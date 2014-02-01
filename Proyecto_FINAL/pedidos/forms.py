6e980e0c96b27c1a5e28351501f00b756035211b
Fernando Llodra Belda <FLlodraB@MacBook-Pro-de-Fernando.local>
2014-02-01 13:16:27 +0100

    Subo versión final del proyecto. Se han corregido los últimos detalles. closes #13

---
+++ Proyecto_FINAL/pedidos/forms.py
@@ -0,0 +1,26 @@
+from django import forms
+from models import Usuarios, Pedidos, Estados, EstadosPedidos
+
+class UsuariosForm(forms.ModelForm):
+	class Meta:
+             password = forms.CharField(widget=forms.PasswordInput)
+             model = Usuarios
+             widgets = {
+                 'password': forms.PasswordInput(),
+             }
+
+class PedidosForm(forms.ModelForm):
+	class Meta:
+		model = Pedidos
+
+class LoginForm(forms.Form):
+    nombre = forms.CharField(max_length=100)
+    password = forms.CharField(widget=forms.PasswordInput)
+    
+class EstadosForm(forms.ModelForm):
+	class Meta:
+		model = Estados
+  
+class SearchPedidoForm(forms.ModelForm):
+	class Meta:
+		model = EstadosPedidos
---
+++ Proyecto_FINAL/pedidos/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+
+# Create your models here.
+
+class Usuarios (models.Model):
+	nombre = models.CharField (max_length=100)
+	correo_electronico = models.CharField (max_length=300)
+	password = models.CharField (max_length=30)
+	rol = models.CharField (max_length=30)
+	
+	def __unicode__(self):
+		return self.nombre
+
+class Estados (models.Model):
+	nombre_estado = models.CharField (max_length=30)
+	
+	def __unicode__(self):
+		return self.nombre_estado
+  
+class Pedidos (models.Model):
+	usuario = models.ForeignKey (Usuarios)
+	num_pedido = models.CharField (max_length=15)
+	concepto = models.CharField(max_length=200)
+	estado = models.ForeignKey (Estados)
+	telefono_tecnico = models.CharField (max_length=12, blank=True)
+	#fecha_instalacion = models.DateTimeField (null=True, blank=True)
+	forma_de_recepcion = models.CharField (max_length=30, blank=True)
+	
+	def __unicode__(self):
+		return self.num_pedido
+		
+class EstadosPedidos (models.Model):
+	num_pedido = models.ForeignKey (Pedidos)
+	estado = models.ForeignKey (Estados)
+	
+	def __unicode__(self):
+		return self.num_pedido
